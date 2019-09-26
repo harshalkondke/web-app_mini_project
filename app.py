60175319b0d6e6080738c909877a99ee6951a207
harshal <me@harshal.codes>
2019-09-26 20:45:27 +0530

    initial image

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ adminid = "admin"
 adminpass = "adminpass"
 
 
-# responses aren't from cached
+# responses aren't from cached in the browser
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
