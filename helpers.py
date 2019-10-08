c9af0afc94f18984905070cf15dbf2759176cce2
Harshal Kondke <52579686+harshalkondke@users.noreply.github.com>
2019-10-08 21:56:32 +0530

    updated

--- helpers.py
+++ helpers.py
@@ -30,13 +30,3 @@ def login_required(f):
         return f(*args, **kwargs)
 
     return decorated_function
-
-def login_required_admin(f):
-
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if (session.get("user_id") == "admin"):
-            return redirect("/admin_panel")
-        return f(*args, **kwargs)
-
-    return decorated_function
